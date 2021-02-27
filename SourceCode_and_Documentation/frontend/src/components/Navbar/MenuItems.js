5f889e4bbe03b43986f1261975b82b7d5f7c5e47
Aimen <aimenhamed@live.com.au>
2021-02-27 22:55:06 +1100

    Added navbar component

---
+++ SourceCode_and_Documentation/frontend/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Drinks',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Account',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
