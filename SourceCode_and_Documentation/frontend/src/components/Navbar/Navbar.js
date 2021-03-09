858996cde276c81e1c7a7a6f056f00d00d6ba860
Aimen <aimenhamed@live.com.au>
2021-03-09 18:32:14 +1100

    New navbar

--- SourceCode_and_Documentation/frontend/src/components/Navbar/Navbar.js
+++ SourceCode_and_Documentation/frontend/src/components/Navbar/Navbar.js
@@ -6,31 +6,34 @@ import { Button } from '../Button'
 
 
 class Navbar extends Component {
-    state = { clicked: false }
-
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
 
     render() {
         return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Liqueur<i className="fab fa-react"></i></h1> 
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {MenuItems.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <Link className={item.cName} to={item.url}>
-                                    {item.title}
-                                </Link>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Liquer</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Shopping List</a>
                             </li>
-                        )
-                    })}
-                </ul>
-                <Button>Sign Up</Button>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Drinks</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Ingredients</a>
+                            </li>
+                        </ul>
+                        <ul class="nav justify-content-end">
+                            <span class="navbar-text justify-content-end">
+                                - Cognac Bandits Project
+                            </span>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         )
     }
