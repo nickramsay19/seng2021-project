3cdd52b2dd7c8c982ec4ab4dd2fcc7ac70aa1619
Henry Ho <z5312521@ad.unsw.edu.au>
2021-03-10 15:56:04 +1100

    added instructions field to dummy cocktails

--- SourceCode_and_Documentation/frontend/src/components/Searchbar/Cocktails.js
+++ SourceCode_and_Documentation/frontend/src/components/Searchbar/Cocktails.js
@@ -5,21 +5,32 @@ export const Cocktails = [
             'Tequilla',
             'Triple sec',
             'Lime juice'
-        ]
+        ],
+        instructions: `Rub the rim of the glass with the lime slice to make the salt stick to it.
+        Take care to moisten only the outer rim and sprinkle the salt on it.
+        The salt should present to the lips of the imbiber and never mix into the cocktail.
+        Shake the other ingredients with ice, then carefully pour into the glass.`
     },
     {
         name: 'Martini',
         ingredients: [
             'Gin',
             'Dry vermouth'
-        ]
+        ],
+        instructions: `Straight: Pour all ingredients into mixing glass with ice cubes.
+        Stir well.
+        Strain in chilled martini cocktail glass.
+        Squeeze oil from lemon peel onto the drink, or garnish with olive.`
     },
     {
         name: 'Gin and Tonic',
         ingredients: [
             'Gin',
             'Tonic water'
-        ]
+        ],
+        instructions: `Pour the gin and the tonic water into a highball glass almost filled with ice cubes.
+        Stir well.
+        Garnish with the lime wedge.`
     },
     {
         name: 'Pina Colada',
@@ -29,7 +40,9 @@ export const Cocktails = [
             'White rum',
             'Sugar syrup',
             'Ice'
-        ]
+        ],
+        instructions: `Mix with crushed ice in blender until smooth.
+        Pour into chilled glass, garnish and serve.`
     },
     {
         name: 'Mojito',
@@ -39,14 +52,20 @@ export const Cocktails = [
             'Soda water',
             'Sugar syrup',
             'Mint'
-        ]
+        ],
+        instructions: `Muddle mint leaves with sugar and lime juice.
+        Add a splash of soda water and fill the glass with cracked ice.
+        Pour the rum and top with soda water.
+        Garnish and serve with straw.`
     },
     {
         name: 'Irish Coffee',
         ingredients: [
             'Irish Whiskey',
             'Coffee'
-        ]
+        ],
+        instructions: `Heat the coffee, whiskey and sugar; do not boil.
+        Pour into glass and top with cream; serve hot.`
     },
     {
         name: 'Espresso Martini',
@@ -55,7 +74,8 @@ export const Cocktails = [
             'Vodka',
             'Sugar syrup',
             'Coffee Liqueur'
-        ]
+        ],
+        instructions: `Pour ingredients into shaker filled with ice, shake vigorously, and strain into chilled martini glass`
     },
     {
         name: 'Gin Fizz',
@@ -65,7 +85,11 @@ export const Cocktails = [
             'Sugar syrup',
             'Egg',
             'Soda water'
-        ]
+        ],
+        instructions: `Shake all ingredients with ice cubes, except soda water.
+        Pour into glass.
+        Top with soda water.`,
+
     },
     {
         name: 'Pisco Sour',
@@ -73,6 +97,7 @@ export const Cocktails = [
             'Pisco',
             'Lime juice',
             'Egg'
-        ]
+        ],
+        instructions: `Vigorously shake and strain contents in a cocktail shaker with ice cubes, then pour into glass and garnish with bitters.`
     }
 ]
