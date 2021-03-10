9d811dff85fbddf2b4ddda33a7246969bea502a5
Jingbang Zhou <jingbang.zhou@unsw.edu.au>
2021-03-10 23:46:34 +1100

    Commenting system first implemented

--- SourceCode_and_Documentation/frontend/src/components/Item-info/item-info.jsx
+++ SourceCode_and_Documentation/frontend/src/components/Item-info/item-info.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import './item-info.css';
 import { Link } from 'react-router-dom';
 
+function PostComt() {
+    var content=document.getElementById("floatingTextarea");
+    var msgBox=document.getElementById("messageBox");
+    if (content.value==="") {
+        return alert("the comment cannot be empty");
+    }
+
+    var commentPost=document.createElement("div");
+    commentPost.className="cmtBlock";
+    commentPost.innerHTML=content.value;
+    msgBox.appendChild(commentPost);
+}
+
 class ItemInfo extends Component {
     state = {  }
     render() { 
@@ -37,9 +50,21 @@ class ItemInfo extends Component {
                     </p>
                 </div>
                 
-                <p>Add to Shopping List</p>   
+                <p>Add to Shopping List</p>
+                
+                <div className="comm">
+                    <div className="form-floating">
+                        <h3 className="item-header">
+                            Comments
+                        </h3>
+                        <textarea class="form-control" placeholder="Add an anonymous comment..." id="floatingTextarea"></textarea>
+                        <button type="button" class="btn btn-outline-primary float-end" onClick={PostComt}>Post</button>                
+                    </div>
+                </div>
+                <div class="message_box" id="messageBox"></div>
+                
             </div>
-         );
+        );
     }
 }
  
