d3c3dd47cbecca3940f09a85df128a52b6959e56
Nicholas Ramsay <nick@yousponsor.me>
2021-03-11 20:31:15 +1100

    adding shopping list page with deletion

---
+++ SourceCode_and_Documentation/frontend/src/components/Shopping_List/IngredientsList.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
+ 
+class IngredientsList extends Component {
+    static propTypes = {
+        cookies: instanceOf(Cookies).isRequired
+    };
+    
+    constructor(props) {
+        super(props);
+    
+        // get cookies
+        const { cookies } = props;
+
+        // add mock ingredients to cookies
+        //cookies.set('ingredients', ['Tequilla', 'Lime juice', 'Triple sec'], { path: '/' });
+
+        // set state
+        this.state = {
+            ingredients: /*cookies.get('ingredients') || */['Tequilla', 'Lime juice', 'Triple sec']
+        };
+    }
+ 
+    // handle ingredient removal
+    removeIngredient = ingredient => {
+
+        // get cookies
+        const { cookies } = this.props;
+            
+        let new_ingredients = [];
+
+        for(let i = 0; i < this.state.ingredients.length; i++){
+            if(ingredient != this.state.ingredients[i]) {
+                new_ingredients.push(this.state.ingredients[i])
+            }
+        }
+        cookies.set('ingredients', new_ingredients, { path: '/' });
+        this.setState({ ingredients: new_ingredients });
+    }
+ 
+    render() {
+        return (
+            <ul className="list-group list-group-flush">
+                {   
+                    this.state.ingredients.map((ingredient, index) =>   
+                        <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+                            {ingredient} 
+                            <button className="btn btn-danger" onClick={() => this.removeIngredient(ingredient)}>
+                                Remove
+                            </button>
+                        </li>
+                    ) 
+                }
+            </ul>
+        );
+    }
+}
+ 
+export default withCookies(IngredientsList);
