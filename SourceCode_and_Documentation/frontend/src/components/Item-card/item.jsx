63d7ca8b5e26325d0b8683102f718bf6630db133
Aimen <aimenhamed@live.com.au>
2021-03-11 21:26:29 +1100

    Added ingredient mapping for /ingredients

--- SourceCode_and_Documentation/frontend/src/components/Item-card/item.jsx
+++ SourceCode_and_Documentation/frontend/src/components/Item-card/item.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './item.css'
 class Item extends Component {
     render() { 
+        // console.log(this.props);
         return ( 
             <div className="item-card">
                 <div className="item-card-image"></div>
