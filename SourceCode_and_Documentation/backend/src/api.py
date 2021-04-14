8bc3f1f02b8c7d6d789696050d14d5238bc5bd53
Henry Ho <z5312521@ad.unsw.edu.au>
2021-04-14 23:35:46 +1000

    changed timeout to 1; added image API calls

--- SourceCode_and_Documentation/backend/src/api.py
+++ SourceCode_and_Documentation/backend/src/api.py
@@ -4,25 +4,21 @@ import concurrent.futures
 import inspect
 from pprint import pprint 
 
-DEFAULT_TIMEOUT = 10
+DEFAULT_TIMEOUT = 1
 
 def api_get_cocktail_details(id, position):
     url = 'http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id
-    print('fetching', url, 'position', position)
+    # print('fetching', url, '; position:', position)
     try:
         r = requests.get(url, timeout=DEFAULT_TIMEOUT).json()['drinks'][0]
     except requests.exceptions.ConnectTimeout:
         # print('position', position)
-        return None
-
-    # if r.status_code != 200:
-    #     return None
-    print('received response', id, 'position', position)
+        return
+    # print('received response from', id, 'in position', position)
     return r
 
 
-def api_get_cocktails():
-    cocktails = requests.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail').json()['drinks']
+def api_get_cocktails(cocktails):
     cocktails_details = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
         future_to_url = (executor.submit(api_get_cocktail_details, cocktail['idDrink'], i) for i, cocktail in enumerate(cocktails))
@@ -48,13 +44,13 @@ def extract_ingredients(cocktails):
     return ingredients
 
 def api_get_ingredient_details(name):
-    name = name.replace(" ", "%20")
     url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=' + name
-    print('fetching', url, 'name', name)
+    # print('fetching', url)
     try:
         ingredient_details = requests.get(url, timeout=DEFAULT_TIMEOUT).json()['ingredients'][0]
-    except requests.exceptions.ReadTimeout:
-        ingredient_details = requests.get(url, timeout=DEFAULT_TIMEOUT).json()['ingredients'][0]
+    except requests.exceptions.ConnectTimeout:
+        return
+    # print('received response from', name)
     return ingredient_details
 
 
@@ -73,17 +69,39 @@ def api_get_ingredients(ingredients):
                 ingredients_details.append(data)
     return ingredients_details
 
-cocktails = api_get_cocktails()
+# Untested
+def api_get_ingredient_image_small(name):
+    # 100x100 pixels
+    url = 'www.thecocktaildb.com/images/ingredients/'+name+'-Small.png'
+    r = requests.get(url)
+    return r
 
-pprint(sorted(cocktails, key=lambda x: x['strDrink']))
+# UNTESTED
+def api_get_ingredient_image_medium(name):
+    # 350x350 pixels
+    url = 'www.thecocktaildb.com/images/ingredients/'+name+'-Medium.png'
+    r = requests.get(url)
+    return r
 
-ingredients = extract_ingredients(cocktails)
+# UNTESTED
+def api_get_ingredient_image(name):
+    # 700x700 pixels
+    url = 'www.thecocktaildb.com/images/ingredients/'+name+'.png'
+    r = requests.get(url)
+    return r
 
-# pprint(sorted(ingredients))
-# print('Total ingredients', len(ingredients))
+cocktails = requests.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail').json()['drinks']
 
-ingredients_details = api_get_ingredients(ingredients)
-# pprint(ingredients_details)
+cocktails_details = api_get_cocktails(cocktails) # list of objects
 
-print('Collected', len(cocktails), 'cocktails out of 100 in', DEFAULT_TIMEOUT, 'seconds')
+ingredients = extract_ingredients(cocktails_details) # list of strings
+
+ingredients_details = api_get_ingredients(ingredients) # list of objects
+
+# printing all lists. comment out if too spammy
+pprint(sorted(cocktails_details, key=lambda x: x['strDrink'].casefold()))
+pprint(sorted(ingredients))
+pprint(sorted(ingredients_details, key=lambda x: x['strIngredient'].casefold()))
+
+print('Collected', len(cocktails_details), 'cocktails out of 100 in', DEFAULT_TIMEOUT, 'seconds')
 print('Received', len(ingredients_details), 'ingredient details out of', len(ingredients), 'in', DEFAULT_TIMEOUT * 2, 'seconds.' )
