d9f13ae8a48df4bea0ddb3ea19d16653bcf7a48d
mahami1 <mihailgeorgiev338@gmail.com>
2021-04-15 21:00:05 +1000

    created methods for login system

---
+++ SourceCode_and_Documentation/backend/src/login_system.py
@@ -0,0 +1,19 @@
+import api
+import server 
+
+u_id = 0
+
+
+def user_login(email, password): 
+    
+def user_register(email, password):
+
+def shoppinglist_add(userID, ingredient):
+
+def shoppinglist_remove(userID, ingredient):
+
+def shoppinglist_get(userID):
+
+def main():
+
+main()
