d9e426b6f7f26f7ebf6b2c17d2d116fd790e9fd8
Nicholas Ramsay <nick@yousponsor.me>
2021-04-17 13:38:35 +1000

    replacing add all ingredients link with button to remove url hash addition

--- SourceCode_and_Documentation/frontend/src/components/Item-info/item-info.jsx
+++ SourceCode_and_Documentation/frontend/src/components/Item-info/item-info.jsx
@@ -104,7 +104,7 @@ class Cocktail extends Component {
                     <h1 className="item-title">
                         {cocktail.name}
                     </h1>
-                    <a href="#" className="btn btn-shortened btn-outline-primary" onClick={() => this.addIngredients(cocktail) }>Add to Shopping List</a>  
+                    <button className="btn btn-shortened btn-outline-primary" onClick={() => this.addIngredients(cocktail) }>Add to Shopping List</button>  
                     
                     <Link to="/drinks"><a href="#" className="btn btn-shortened btn-outline-primary">To Drinks Page</a></Link>
                 </div>
