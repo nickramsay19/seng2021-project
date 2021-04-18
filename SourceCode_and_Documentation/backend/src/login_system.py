fd51abafe1edc78574ef2eead3ff4fcb1223e7d7
Nicholas Ramsay <nick@yousponsor.me>
2021-04-18 20:10:53 +1000

    added returns to login_system methods for error catching

--- SourceCode_and_Documentation/backend/src/login_system.py
+++ SourceCode_and_Documentation/backend/src/login_system.py
@@ -5,11 +5,8 @@ import data
 u_id = 0
 
 def user_register(username, password):
-    
     data.users.append({'user': username, 'password': password, 'u_id': len(data.users), 'logged_in': False, 'shopping_list': []}) #data.users.count()
     
-
-
 def user_login(username, password): 
     for i in data.users:
         if ((i['user'] == username) and (i['password'] == password) and (i['logged_in'] == False)):
@@ -21,16 +18,21 @@ def shoppinglist_add(userID, ingredient):
     for i in data.users:
         if ((i['user'] == userID) and (i['logged_in'] == True)):
             i['shopping_list'].append(ingredient)
+            return True
+    return False
 
 def shoppinglist_remove(userID, ingredient):
     for i in data.users:
         if ((i['user'] == userID) and (i['logged_in'] == True)):
             i['shopping_list'].remove(ingredient)
+            return True
+    return False
 
 def shoppinglist_get(userID):
     for i in data.users:
         if ((i['user'] == userID) and (i['logged_in'] == True)):
             return i['shopping_list']
+    return None
 
 def main():
     user_register('user1', 'password1')
