2c579bc0dd998af7460824c98dff356e26dd5326
Henry Ho <z5312521@ad.unsw.edu.au>
2021-04-18 15:39:47 +1000

    show blank template instead of crashing when opening unknown drink

--- SourceCode_and_Documentation/frontend/src/components/Ingredient-info/ingredient-info.jsx
+++ SourceCode_and_Documentation/frontend/src/components/Ingredient-info/ingredient-info.jsx
@@ -6,23 +6,29 @@ class IngredientInfo extends Component {
         const { match } = this.props;
         const { ingredientID } = match.params;
         console.log(this.props.ingredients);
-        const ingredient = this.props.ingredients.find((i) => {
+        var ingredient = this.props.ingredients.find((i) => {
             if (i.ingredient === ingredientID) {
                 return true
             }
         })
+        if (typeof ingredient === 'undefined' ) {
+            ingredient = {
+                ingredient : '',
+                description : '', 
+            }
+        }
         console.log(ingredient)
         return(
             <div className="item-info-card">
                 <div className="item-top-bar">
                     <h1 className="item-title">
-                        {ingredient.ingredient || "Item Title"}
+                        {ingredient.ingredient || "Not in database"}
                     </h1>
                     <Link to="/ingredients"><p className="btn btn-outline-primary">Back</p></Link>
                 </div>
                 
                 <div className="item-row">
-                    <img className="item-card-image" src="http://www.thecocktaildb.com/images/ingredients/gin.png"></img>
+                    <img className="item-card-image" src={`http://www.thecocktaildb.com/images/ingredients/${ingredient.ingredient}.png`}></img>
                     <div className="item-ingredients">
                         <h2 className="item-header">
                             Description
