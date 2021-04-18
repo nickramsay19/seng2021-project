ae11bc550730a58d66230e8f5f44f99fcebc6259
Ben Zhou <bzhou@192-168-1-142.tpgi.com.au>
2021-04-18 18:33:44 +1000

    minor fixes

--- SourceCode_and_Documentation/backend/src/comments_test.py
+++ SourceCode_and_Documentation/backend/src/comments_test.py
@@ -1,22 +1,38 @@
 import data
 from comments import*
+from login_system import*
 
 data.clear()
 
+@pytest.fixture
+def users():
+    flockr.clear()
+    'creates two users'
+    return [
+        user_register('user1', 'password1'),
+        user_register('user2', 'password2'),
+        user_register('user3', 'password3'),
+        user_register('user4', 'password4')
+    ]
+
+
 '''
-Testing for comment_add
+Testing for comments_add
 '''
-def test_comment_add():
+def test_comments_add(users):
+    assert user_login('user1', 'password1') == True
+    assert user_login('user3', 'password3') == True
+    assert comments_add()
+
+
+'''
+Testing for comments_remove
+'''
+def test_comments_remove(users):
     pass
 
 '''
-Testing for comment_remove
+Testing for comments_get
 '''
-def test_comment_remove():
-    pass
-
-'''
-Testing for comment_get
-'''
-def test_comment_get():
+def test_comments_get(users):
     pass
