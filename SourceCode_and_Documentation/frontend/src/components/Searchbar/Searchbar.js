a30e4e0862fdf476aa2352bf8b66d7480b794356
Nicholas Ramsay <nick@yousponsor.me>
2021-04-19 18:27:39 +1000

    removed redundant Cocktails.js import, removed bug where only one cocktail can show

--- SourceCode_and_Documentation/frontend/src/components/Searchbar/Searchbar.js
+++ SourceCode_and_Documentation/frontend/src/components/Searchbar/Searchbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import stringSimilarity from 'string-similarity';
-import { Cocktails } from '../Drinks/Cocktails'
 import './Searchbar.css';
 
 
@@ -53,7 +52,7 @@ class Searchbar extends Component {
         return [...new Set(ingredients)];
     }
 
-    onSearchQueryChange2 = query => {
+    onSearchQueryChange = query => {
 
         // obtain a copy of drinks and ingredients
         let drinks = this.getMinifiedDrinks();
@@ -85,74 +84,7 @@ class Searchbar extends Component {
             // check against cocktail name
             for (let d = 0; d < drinks.length; d++) {
                 if(stringSimilarity.compareTwoStrings(query_words[w].toLowerCase(), drinks[d].toLowerCase()) > 0.7){
-
                     results.push(drinks[d])
-                    
-                    // break the for loop, ingredient has already been added
-                    break;
-                }
-            }
-        }
-
-        console.log(results.length);
-
-        // set search_results
-        // remove duplicates by converting to a set then arr again
-        this.setState({
-            search_results: [...new Set(results)],
-        })
-        
-    }
-
-    // updates search_results based on search bar input
-    onSearchQueryChange = (query) => {
-
-        // split query into separate word searches
-        let query_words = query.split(" ");
-
-        // declare a new results array
-        let results = [];
-        
-        // loop through each query word
-        for(let i = 0; i < query_words.length; i++) {
-
-            // loop through each cocktail
-            for(let j = 0; j < Cocktails.length; j++) {
-
-                // split cocktail into separate words
-                let cocktails_words = Cocktails[j].name.split(" ")
-
-                // loop through each word in cocktail name
-                for(let k = 0; k < cocktails_words.length; k++) {
-
-                    // perform a string similarity check between query (to lower case) and cocktail name (to lower case)
-                    if(stringSimilarity.compareTwoStrings(query_words[i].toLowerCase(), cocktails_words[k].toLowerCase()) > 0.7){
-
-                        // append the cocktail to search_results
-                        results.push(Cocktails[j]);
-
-                        // break the for loop, cocktail has already been added
-                        break;
-                    }
-                    // loop through ingredients
-                    for(let l = 0; l < Cocktails[j].ingredients.length; l++){
-
-                        // split cocktail ingredient into separate words
-                        let ingredient_words = Cocktails[j].ingredients[l].split(" ")
-
-                        for(let m = 0; m < ingredient_words.length; m++) {
-                            // perform a string similarity check between query (to lower case) and all cocktail ingredients (to lower case)
-                            if(stringSimilarity.compareTwoStrings(query_words[i].toLowerCase(), ingredient_words[m].toLowerCase()) > 0.7){
-
-                                // append the cocktail to search_results
-                                results.push(Cocktails[j]);
-
-                                // break the for loop, cocktail has already been added
-                                break;
-                            }
-                        }
-                        
-                    }
                 }
             }
         }
@@ -162,13 +94,14 @@ class Searchbar extends Component {
         this.setState({
             search_results: [...new Set(results)],
         })
+        
     }
 
     render() {
         return(
             <div id="searchbar">
                 <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Margarita" onChange={e => this.onSearchQueryChange2(e.target.value)} /> 
+                    <input type="text" className="form-control" placeholder="Margarita" onChange={e => this.onSearchQueryChange(e.target.value)} /> 
                 </div>
                 
                 <ul className="list-group">
