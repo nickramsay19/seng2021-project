890a5cfc15c3cf8659ba7edaff7635f05882f16a
Nicholas Ramsay <nick@yousponsor.me>
2021-04-19 21:31:25 +1000

    added register and logout linking to backend

--- SourceCode_and_Documentation/frontend/src/userSession.js
+++ SourceCode_and_Documentation/frontend/src/userSession.js
@@ -26,6 +26,34 @@ function createUserSession(update){
         Cookies.set('userId', userId, { expires: 7});
     }
     
+    function register(name, password, _callback) {
+
+        let req = `http://127.0.0.1:5050/auth/register?username=${name}&password=${password}`;
+        console.log(req);
+        fetch(req, {method: 'post'})
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                if (res?.data != -1) {
+                    isLoggedIn = true;
+                    username = name;
+                    userId = res.data;
+
+                    // set cookies
+                    updateCookies();
+
+                    // update
+                    callUpdate();
+
+                    // return true
+                    _callback();
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+    }
+
     function logIn(name, password, _callback) {
 
         let req = `http://127.0.0.1:5050/auth/login?username=${name}&password=${password}`;
@@ -55,6 +83,33 @@ function createUserSession(update){
     }
 
     function logOut() {
+
+        let req = `http://127.0.0.1:5050/auth/logout?username=${username}`;
+        console.log(req);
+        fetch(req, {method: 'post'})
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                if (res?.data >= 0) {
+                    isLoggedIn = false;
+                    username = "";
+                    userId = -1;
+
+                    // set cookies
+                    updateCookies();
+
+                    // update
+                    callUpdate();
+
+                    // return true
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+
+        // old
         isLoggedIn = false;
         username = "";
         userId = -1;
@@ -70,7 +125,7 @@ function createUserSession(update){
         return isLoggedIn;
     }
 
-    return {logIn, logOut, getIsLoggedIn}
+    return {register, logIn, logOut, getIsLoggedIn}
 }
 
 export default createUserSession
